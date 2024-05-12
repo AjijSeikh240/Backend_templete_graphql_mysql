67c65d8d49f146364e16644d64d681a739b21eb2
Ajij Seikh 240 <ajijplacement240@gmail.com>
2024-05-12 11:25:01 +0530

    add all file

---
+++ models/user.model.js
@@ -0,0 +1,26 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  User.associate = function (models) {};
+  return User;
+};
+
+export default User;
